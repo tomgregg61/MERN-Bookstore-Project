083fa2059b02d05579b9dd68af880a24a8e9b710
Tom Gregg <thomasgregg05@outlook.com>
2024-05-14 22:08:35 +0100

    Add React and Tailwind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import {Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBooks';
+import ShowBook from "./pages/ShowBook";
+import EditBook from "./pages/EditBook";
+import DeleteBook from "./pages/DeleteBook";
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="" element={}/>
+      <Route path="" element={}/>
+      <Route path="" element={}/>
+      <Route path="" element={}/>
+      <Route path="" element={}/>
+    </Routes>
+  )
+}
+
+export default App;
