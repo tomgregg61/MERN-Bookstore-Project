b77df644173be04001d332ec0a1b2801fa3f06a7
Tom Gregg <thomasgregg05@outlook.com>
2024-05-14 22:14:38 +0100

    create spinner

--- backend/index.js
+++ backend/index.js
@@ -8,15 +8,15 @@ const app = express();
 
 app.use(express.json());
 
-// app.use(cors());
+app.use(cors());
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// );
 
 app.get("/", (request, response) => {
   console.log(request);
