f0362b57059ea6c0d1f714ea447a40dd07ec0c07
Tom Gregg <thomasgregg05@outlook.com>
2024-05-14 21:21:17 +0100

    Intitial upload

---
+++ backend/config.js
@@ -0,0 +1,4 @@
+export const PORT = 5555;
+
+export const mongoDBURL =
+  "mongodb+srv://root:root@book-store-mern.j7p8vk2.mongodb.net/books-collection?retryWrites=true&w=majority&appName=Book-Store-MERN";
